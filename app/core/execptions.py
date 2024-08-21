51ed4a44361f2a1e0c672439ef48076cf231b936
songszw <songszw@gmail.com>
2024-08-21 14:13:05 +0800

    修改异常提示信息， 方便前端国际化

--- app/core/execptions.py
+++ app/core/execptions.py
@@ -1,41 +1,41 @@
 class UserEmailAlreadyExistsError(Exception):
-    def __init__(self, message="User already exists"):
+    def __init__(self, message="EMAIL_ALREADY_EXISTS"):
         self.message = message
         super().__init__(self.message)
 
 
 class UsernameAlreadyExistsError(Exception):
-    def __init__(self, message="Username already exists"):
+    def __init__(self, message="USERNAME_ALREADY_EXISTS"):
         self.message = message
         super().__init__(self.message)
 
 
 class LoginError(Exception):
-    def __init__(self, message="Incorrect username or password"):
+    def __init__(self, message="INCORRECT_USERNAME_OR_PASSWORD"):
         self.message = message
         super().__init__(self.message)
 
 
 class CategoryAlreadyExistsError(Exception):
-    def __init__(self, message="Category already exists"):
+    def __init__(self, message="CATEGORY_ALREADY_EXISTS"):
         self.message = message
         super().__init__(self.message)
 
 
 class CategoryNotFoundError(Exception):
-    def __init__(self, message="Category not found"):
+    def __init__(self, message="CATEGORY_NOT_FOUND"):
         self.message = message
         super().__init__(self.message)
         
 
 class CategoryDeleteError(Exception):
-    def __init__(self, message="Category delete error"):
+    def __init__(self, message="CATEGORY_DELETE_ERROR"):
         self.message = message
         super().__init__(self.message)
 
 
 class EntryNotFoundError(Exception):
-    def __init__(self, message="Entry not found"):
+    def __init__(self, message="ENTRY_NOT_FOUND"):
         self.message = message
         super().__init__(self.message)
 
@@ -47,12 +47,12 @@ class CustomException(Exception):
 
 
 class PasswordError(Exception):
-    def __init__(self, message="Current password is incorrect"):
+    def __init__(self, message="CURRENT_PASSWORD_IS_INCORRECT"):
         self.message = message
         super().__init__(self.message)
 
 
 class UserNotFoundError(Exception):
-    def __init__(self, message="User not found"):
+    def __init__(self, message="USER_NOT_FOUND"):
         self.message = message
         super().__init__(self.message)
